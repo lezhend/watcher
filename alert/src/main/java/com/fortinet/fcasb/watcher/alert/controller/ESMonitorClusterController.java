fe2dcccaaaa19fcac301df96e810d565e1eb22b8
Alvin Xie <alvinxie@fortinet.com>
2017-05-08 16:24:12 +0800

    Change to use autowired TransportClient

--- alert/src/main/java/com/fortinet/fcasb/watcher/alert/controller/ESMonitorClusterController.java
+++ alert/src/main/java/com/fortinet/fcasb/watcher/alert/controller/ESMonitorClusterController.java
@@ -1,7 +1,6 @@
 package com.fortinet.fcasb.watcher.alert.controller;
 
 import java.io.IOException;
-import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 import org.apache.http.client.ClientProtocolException;
@@ -10,9 +9,7 @@ import org.elasticsearch.action.admin.cluster.node.info.NodesInfoResponse;
 import org.elasticsearch.action.admin.cluster.stats.ClusterStatsResponse;
 import org.elasticsearch.client.ClusterAdminClient;
 import org.elasticsearch.client.transport.TransportClient;
-import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.transport.InetSocketTransportAddress;
-import org.elasticsearch.transport.client.PreBuiltTransportClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,6 +22,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/esmonitor/cluster")
 public class ESMonitorClusterController {
+	
+	@Autowired
+    private TransportClient client;
 
 	@Value("${es.transport.client.host}")
 	public String esServerHost;
@@ -35,14 +35,8 @@ public class ESMonitorClusterController {
 	@Value("${es.cluster.name}")
 	public String esClusterName;
 	
-	@SuppressWarnings("resource")
 	@RequestMapping(value = "/health", method = RequestMethod.GET, produces = "application/json")
 	public String health() throws UnknownHostException {
-		byte[] ipInBytes = InetAddress.getByName(esServerHost).getAddress();
-    	Settings settings = Settings.builder()
-    	        .put("cluster.name", esClusterName).build();
-    	TransportClient client = new PreBuiltTransportClient(settings)
-    	        .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByAddress(ipInBytes), esServerPort));
     	ClusterAdminClient clusterAdminClient = client.admin().cluster();
     	ClusterHealthResponse clusterHealth = clusterAdminClient.prepareHealth().get();
     	
@@ -50,42 +44,24 @@ public class ESMonitorClusterController {
 	}
 
 	// get cluster cpu jvm memory
-	@SuppressWarnings("resource")
 	@RequestMapping(value = "/stats", method = RequestMethod.GET, produces = "application/json")
 	public String stats() throws ClientProtocolException, IOException {
-		byte[] ipInBytes = InetAddress.getByName(esServerHost).getAddress();
-    	Settings settings = Settings.builder()
-    	        .put("cluster.name", esClusterName).build();
-    	TransportClient client = new PreBuiltTransportClient(settings)
-    	        .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByAddress(ipInBytes), esServerPort));
     	ClusterAdminClient clusterAdminClient = client.admin().cluster();
     	ClusterStatsResponse clusterStats = clusterAdminClient.prepareClusterStats().get();
     	
     	return clusterStats.toString();
 	}
 	
-	@SuppressWarnings("resource")
 	@RequestMapping(value = "/nodes", method = RequestMethod.GET, produces = "application/json")
 	public String nodes() throws ClientProtocolException, IOException {
-		byte[] ipInBytes = InetAddress.getByName(esServerHost).getAddress();
-    	Settings settings = Settings.builder()
-    	        .put("cluster.name", esClusterName).build();
-    	TransportClient client = new PreBuiltTransportClient(settings)
-    	        .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByAddress(ipInBytes), esServerPort));
     	ClusterAdminClient clusterAdminClient = client.admin().cluster();
     	NodesInfoResponse nodesInfo = clusterAdminClient.prepareNodesInfo().get();
     	
     	return nodesInfo.toString();
 	}
 
-	@SuppressWarnings("resource")
 	@RequestMapping(value = "/nodes/{nodeName}/stats", method = RequestMethod.GET, produces = "application/json")
 	public String nodeStats(@PathVariable("nodeName") String nodeName) throws ClientProtocolException, IOException {
-		byte[] ipInBytes = InetAddress.getByName(esServerHost).getAddress();
-    	Settings settings = Settings.builder()
-    	        .put("cluster.name", esClusterName).build();
-    	TransportClient client = new PreBuiltTransportClient(settings)
-    	        .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByAddress(ipInBytes), esServerPort));
     	ClusterAdminClient clusterAdminClient = client.admin().cluster();
     	NodesInfoResponse nodesInfo = clusterAdminClient.prepareNodesInfo(nodeName).get();
     	
