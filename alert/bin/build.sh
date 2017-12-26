705fe7d9e9f9cf4ff13b56f6428430f7ce321097
zliu@fortinet.com <zliu@fortinet.com>
2017-12-26 17:01:07 +0800

    add Dockerfile and taskdef.json

--- alert/bin/build.sh
+++ alert/bin/build.sh
@@ -1,14 +1,31 @@
 #!/bin/bash
-PRO_NAME="alert"
-VER_NAME="1.0"
-echo "Build module ["$PRO_NAME $VER_NAME"]=============="
+#Constants
+REGION="us-west-2"
+REPOSITORY_NAME="watcher"
+CLUSTER="logstash-test"
+SERVICE_NAME="watcher"
+FAMILY="alert-springboot"
+NAME="springboot"
+TASKDEFNAME="alert-springboot"
+#Store the repositoryUri as a variable
+REPOSITORY_URI=`aws ecr describe-repositories --repository-names ${REPOSITORY_NAME} --region ${REGION} | jq .repositories[].repositoryUri | tr -d '"'`
+#Replace the build number and respository URI placeholders with the constants above
+sed -e "s;%BUILD_NUMBER%;${BUILD_NUMBER};g" -e "s;%REPOSITORY_URI%;${REPOSITORY_URI};g" taskdef.json > ${NAME}-v_${BUILD_NUMBER}.json
+#Register the task definition in the repository
+aws ecs register-task-definition --family ${FAMILY} --cli-input-json file://${WORKSPACE}/${NAME}-v_${BUILD_NUMBER}.json --region ${REGION}
+SERVICES=`aws ecs describe-services --services ${SERVICE_NAME} --cluster ${CLUSTER} --region ${REGION} | jq .failures[]`
+#Get latest revision
+REVISION=`aws ecs describe-task-definition --task-definition ${TASKDEFNAME} --region ${REGION} | jq .taskDefinition.revision`
 
-mvn clean package -f $1/$PRO_NAME/pom.xml
-if [ $? -gt 0 ];then
-    exit $?
+#Create or update service
+if [ "$SERVICES" == "" ]; then
+  echo "entered existing service"
+  DESIRED_COUNT=`aws ecs describe-services --services ${SERVICE_NAME} --cluster ${CLUSTER} --region ${REGION} | jq .services[].desiredCount`
+  if [ ${DESIRED_COUNT} = "0" ]; then
+    DESIRED_COUNT="1"
+  fi
+  aws ecs update-service --cluster ${CLUSTER} --region ${REGION} --service ${SERVICE_NAME} --task-definition ${FAMILY}:${REVISION} --desired-count ${DESIRED_COUNT}
+else
+  echo "entered new service"
+  aws ecs create-service --service-name ${SERVICE_NAME} --desired-count 1 --task-definition ${FAMILY} --cluster ${CLUSTER} --region ${REGION}
 fi
-aws s3 cp $1/$PRO_NAME/target/$PRO_NAME-$VER_NAME.jar s3://fcasb-data/devops/$PRO_NAME/package/$2/
-aws s3 cp $1/$PRO_NAME/target/$PRO_NAME-$VER_NAME.jar s3://fcasb-data/devops/$PRO_NAME/package/
-aws s3 cp $1/$PRO_NAME/bin/tools.sh s3://fcasb-data/devops/$PRO_NAME/bin/tools.sh
-aws s3 cp $1/$PRO_NAME/src/main/resources/$PRO_NAME.properties s3://fcasb-data/devops/$PRO_NAME/config/
-aws s3 cp $1/$PRO_NAME/src/main/resources/logback.xml s3://fcasb-data/devops/$PRO_NAME/config/
