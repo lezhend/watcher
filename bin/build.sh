57f23a3c2dda368f6d3d5e575d39137ec5c11ac3
zliu@fortinet.com <zliu@fortinet.com>
2017-12-27 15:40:25 +0800

    add Dockerfile and taskdef.json

--- bin/build.sh
+++ bin/build.sh
@@ -3,7 +3,7 @@ echo "====="$WORKSPACE"==="
 echo "Build parent=============="
 cd alert
 docker build -t alert:v_$BUILD_NUMBER .
-docker tag watcher/alert:v_$BUILD_NUMBER 482025328369.dkr.ecr.us-west-2.amazonaws.com/watcher/alert:latest
+docker tag alert:v_$BUILD_NUMBER 482025328369.dkr.ecr.us-west-2.amazonaws.com/watcher/alert:latest
 docker push 482025328369.dkr.ecr.us-west-2.amazonaws.com/watcher/alert:latest
 /bin/bash $1/alert/bin/build.sh
 if [ $? -gt 0 ];then
