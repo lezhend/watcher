001713f57e54c18d6ee998998f82de3ff5afe1e6
zliu@fortinet.com <zliu@fortinet.com>
2018-01-04 15:50:30 +0800

    add monitor type monitor service

--- alert/src/main/java/com/fortinet/fcasb/watcher/alert/enums/MonitorTypeEnum.java
+++ alert/src/main/java/com/fortinet/fcasb/watcher/alert/enums/MonitorTypeEnum.java
@@ -3,11 +3,13 @@ package com.fortinet.fcasb.watcher.alert.enums;
 import com.fortinet.fcasb.watcher.alert.task.MonitorESTask;
 import com.fortinet.fcasb.watcher.alert.task.MonitorLogstashTask;
 import com.fortinet.fcasb.watcher.alert.task.MonitorSBTask;
+import com.fortinet.fcasb.watcher.alert.task.MonitorServiceTask;
 
 /**
  * Created by zliu on 17/3/3.
  */
 public enum MonitorTypeEnum {
+    MONITOR_SERVICE(MonitorServiceTask.class),
     SPRING_BOOT(MonitorSBTask.class),
     ES(MonitorESTask.class),
     LOGSTASH(MonitorLogstashTask.class),
