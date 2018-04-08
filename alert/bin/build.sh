fdb6f26125ab1f2819fae34988ce93e339fa5f71
zliu@fortinet.com <zliu@fortinet.com>
2018-04-08 11:37:55 +0800

    improve build.sh

--- alert/bin/build.sh
+++ alert/bin/build.sh
@@ -19,5 +19,5 @@ sed -e "s;%BUILD_NUMBER%;${BUILD_NUMBER};g" -e "s;%REPOSITORY_URI%;${REPOSITORY_
 ssh k8s-master "mkdir -p $K8S_HOME/$MODULE_NAME/"
 scp ${SERVICE_NAME}-v_${BUILD_NUMBER}.yaml k8s-master:$K8S_HOME/$MODULE_NAME/
 
-echo `ssh k8s-master "kubectl apply -f $K8S_HOME/$MODULE_NAME/${SERVICE_NAME}-v_${BUILD_NUMBER}"`
+echo `ssh k8s-master "kubectl apply -f $K8S_HOME/$MODULE_NAME/${SERVICE_NAME}-v_${BUILD_NUMBER}.yaml"`
 echo `ssh k8s-master "kubectl rollout status deployment/${SERVICE_NAME} -n elk-test"`
